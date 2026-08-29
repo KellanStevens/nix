6f3f3eb98de8ce20c28d3fcd474d3a24df83dc88
Kellan Stevens <github@kellanstevens.com>
2026-08-29 21:31:34 +0200

    chore: comment cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,15 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos";
   networking.firewall.allowedTCPPorts = [
     80
     443
@@ -33,7 +27,6 @@
 
   hardware.graphics.enable = true;
 
-  # Enable networking
   networking.networkmanager.enable = true;
   programs.nix-ld.enable = true;
 
@@ -72,10 +65,8 @@
     ];
   };
 
-  # Set your time zone.
   time.timeZone = "Africa/Johannesburg";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_ZA.UTF-8";
 
   # Enable the X11 windowing system.
@@ -83,17 +74,13 @@
 
   programs.helium.enable = true;
 
-  # Enable the Pantheon Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.desktopManager.pantheon.enable = true;
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "za";
     variant = "";
   };
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
   services.pulseaudio.enable = false;
@@ -126,10 +113,6 @@
     };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kellan = {
     isNormalUser = true;
     description = "Kellan Stevens";
@@ -157,24 +140,6 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     settings = {
@@ -191,13 +156,7 @@
   systemd.targets.hybrid-sleep.enable = false;
 
   networking.networkmanager.wifi.powersave = false;
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
+  system.stateVersion = "26.05";
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
