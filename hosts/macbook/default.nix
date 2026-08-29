c48f9a9d988f0c165a2685c57d31061375032aa0
Kellan Stevens <github@kellanstevens.com>
2026-08-29 21:37:11 +0200

    feat(hosts): add host configurations for nixos and macbook

---
+++ hosts/macbook/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  # Starter configuration template for macOS (nix-darwin)
+  # Host-specific macOS configurations can be added here.
+
+  # Example nix-darwin settings:
+  # system.stateVersion = 5;
+  # services.nix-daemon.enable = true;
+}
---
+++ hosts/nixos/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/core
+    ../../modules/nixos
+  ];
+
+  # Bootloader configuration
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Hostname
+  networking.hostName = "nixos";
+
+  # User account configuration
+  users.users.kellan = {
+    isNormalUser = true;
+    description = "Kellan Stevens";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "render"
+    ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGaZl4aI8q4/LSdvABWVesFV1GVaPlWarq1Bl2KbwKl"
+    ];
+  };
+
+  system.stateVersion = "26.05";
+}
