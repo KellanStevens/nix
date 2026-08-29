e59a2beb1079b40a5ec7091c80c91bcf3a513b31
Kellan Stevens <github@kellanstevens.com>
2026-08-29 23:25:13 +0200

    refactor(nixos): flatten module hierarchy and inline trivial service files

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,7 +3,24 @@
 {
   imports = [
     ./desktop.nix
-    ./services.nix
     ./network.nix
+    ./services/home-assistant.nix
+    ./services/ssh.nix
+    ./services/traefik.nix
+    ./services/uxplay.nix
   ];
+
+  # Nix LD support for unpatched binaries
+  programs.nix-ld.enable = true;
+
+  # Avahi mDNS / Zeroconf Service Discovery
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
 }
