582c302d5e2b579e64ec83dacb3922de5d9407a9
Kellan Stevens <github@kellanstevens.com>
2026-08-29 21:37:14 +0200

    refactor(flake): migrate configuration to modular host & home-manager outputs

--- flake.nix
+++ flake.nix
@@ -1,22 +1,56 @@
 {
-  description = "Kellan's Home Server Flake";
+  description = "Kellan's Multi-Host & Cross-Platform Flake Config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     helium.url = "github:oxcl/nix-flake-helium-browser";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      
-      specialArgs = { inherit inputs; };
-      
-      modules = [
-        ./hardware-configuration.nix
-        ./configuration.nix
-        inputs.helium.nixosModules.default
-      ];
+  outputs = { self, nixpkgs, home-manager, darwin, helium, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/nixos
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.kellan = import ./modules/home;
+          }
+        ];
+      };
+    };
+
+    # Template for macOS (nix-darwin) setup when ready
+    darwinConfigurations = {
+      # macbook = darwin.lib.darwinSystem {
+      #   system = "aarch64-darwin"; # or "x86_64-darwin"
+      #   specialArgs = { inherit inputs; };
+      #   modules = [
+      #     ./hosts/macbook
+      #     home-manager.darwinModules.home-manager
+      #     {
+      #       home-manager.useGlobalPkgs = true;
+      #       home-manager.useUserPackages = true;
+      #       home-manager.extraSpecialArgs = { inherit inputs; };
+      #       home-manager.users.kellan = import ./modules/home;
+      #     }
+      #   ];
+      # };
     };
   };
 }
