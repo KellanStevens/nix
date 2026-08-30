3716cef95a71c222fa92a2a6fb427374242a74fd
Kellan Stevens <kellan.stevens@tillo.io>
2026-08-30 15:34:57 +0200

    feat(macos): add new shell aliases for Homebrew and Laravel commands

--- modules/home/macos/aliases.nix
+++ modules/home/macos/aliases.nix
@@ -3,5 +3,17 @@
 {
   home.shellAliases = lib.mkIf pkgs.stdenv.isDarwin {
     nix-rebuild = "sudo darwin-rebuild switch --flake ~/nix#TL-FW21FX96ND";
+    brwup = "brew update -v && brew upgrade -v -g";
+    sail = "[ -f sail ] && sh sail || sh vendor/bin/sail";
+    docker = "AWS_PROFILE=internal-services /opt/homebrew/bin/docker";
+    static-analysis="export PHPSTAN_MAX_PROCESSES=8 composer phpstan && composer phpmd";
+    sp="export PHPSTAN_MAX_PROCESSES=8 composer phpstan && composer phpmd && sail pint";
+    aws-login="aws sso login --sso-session tillo-sso";
+    satcf="sail artisan test --compact --filter=";
+    satc="sail artisan test --compact";
+    satf="sail artisan test --filter=";
+    sat="sail artisan test";
+    pa = "php artisan";
+    pint="sail pint";
   };
 }
